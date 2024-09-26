3068b07aa9d4aa7204da500c4adbd251170d12b0
stoffe-fe23 <kristoffer.bengtsson@gritacademy.se>
2024-09-26 08:13:49 +0200

    justering

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,20 @@ import { Clock } from './Clock';
 function App() {
   // State variable to keep the time to display, defaults to current time.
   const [clockTime, setClockTime] = useState(() => {
-    return getCurrentTime();
+    return getFormattedTime();
   });
 
   // Start interval timer when app starts, triggering callback every second updating the time variable. 
-  useEffect(() => { setInterval(() => setClockTime(getCurrentTime()), 1000); }, []);
+  useEffect(() => { setInterval(() => setClockTime(getFormattedTime()), 1000); }, []);
 
   // Get current time in HH:MM:SS format. 
-  function getCurrentTime(locale = "sv-SE") {
+  function getFormattedTime(dateObj = null, locale = "sv-SE") {
     const formatOptions = {
       hour: "numeric",
       minute: "numeric",
       second: "numeric",
     };
-    return new Intl.DateTimeFormat(locale ?? navigator.language, formatOptions).format(new Date(Date.now()));
+    return new Intl.DateTimeFormat(locale, formatOptions).format(dateObj ?? new Date(Date.now()));
   }
 
   // Component
