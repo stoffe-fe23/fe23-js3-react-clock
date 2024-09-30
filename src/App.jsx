89781ad51c267e438b222bd331c56e9be58de082
stoffe-fe23 <kristoffer.bengtsson@gritacademy.se>
2024-09-30 08:53:09 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
 /*
   Inlämningsuppgift 1, JavaScript 3, Kristoffer Bengtsson (FE23)
-    Main App component. 
+    Main App component, shows Clock component with current time.  
 */
 import { useState, useEffect } from 'react';
 import './App.css';
 import { Clock } from './Clock';
 
 function App() {
-    // State to keep the time to display, defaults to current time.
+    // State to keep the time to display, starts at current time.
     const [clockTime, setClockTime] = useState(() => {
-        return new Date(Date.now());
+        return new Date();
     });
 
     // Start interval timer when App component first loads, triggering callback every second updating the time variable. 
-    // Have to put in useEffect-hook otherwise another new interval timer keeps getting created whenever the component rerenders. 
+    // Put in useEffect-hook to prevent another new interval timer getting created whenever the component rerenders. 
     useEffect(() => {
+        console.log("DEBUG: EFFECT");
         const clockTimer = setInterval(() => {
-            const newTime = new Date(Date.now());
+            const newTime = new Date();
             setClockTime(newTime);
-            console.log("TIME UPDATE:", clockTimer, newTime);
+            console.log("DEBUG: INTERVAL:", clockTimer, newTime);
         }, 1000);
 
-        // Cleanup function to stop interval timer when the App component is unmounted/removed from DOM. 
-        // Also helps to prevent interval from being set twice due to Strict Mode when running in dev mode, 
-        // see: https://legacy.reactjs.org/docs/strict-mode.html#detecting-unexpected-side-effects        
-        return () => clearInterval(clockTimer);
+        // Cleanup function to stop interval timer when the App component is unmounted/removed from DOM.     
+        return () => {
+            console.log("DEBUG: CLEANUP");
+            clearInterval(clockTimer);
+        };
     }, []);
-
 
     // App Component JSX
     return (
